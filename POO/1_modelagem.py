6c4480f9d2384bd1c9f8bd8868ed53742533fb07
kadeguilherme <kadeguilherme@gmail.com>
2024-04-28 18:38:03 -0300

    Atribudos de instancias

--- POO/1_modelagem.py
+++ POO/1_modelagem.py
@@ -1,13 +1,19 @@
 #Criando a classe
 class Pessoas:
+    #Metodo construdor
     def __init__(self, nome, idade, cpf):
+        #Atributos de instancias (self)
         self.nome = nome
         self.idade = idade
         self.cpf = cpf
     
+    def retorna_nome(self):
+        return self.nome
+    
     #Criando metodo
     def logar_sistema(self):
         print(f'{self.nome} está logando no sistema')
+        print(self.retorna_nome())
 
 #Criando instancia
 p1 = Pessoas('Caio', 21, '123-456-789')
